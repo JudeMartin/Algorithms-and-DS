1f9e7c560ef78e217b8725125a2b18263181c8fa
Jude Martin <judemartin.mj@gmail.com>
2015-12-29 23:30:29 -0800

    HackerRank - WarmUp

---
+++ src/HackerRank/Algorithms/warmup/AVeryBigSum.java
@@ -0,0 +1,21 @@
+/* https://www.hackerrank.com/challenges/a-very-big-sum */
+
+package HackerRank.Algorithms.warmup;
+
+import java.util.Scanner;
+
+public class AVeryBigSum {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int arr[] = new int[n];
+		for (int arr_i = 0; arr_i < n; arr_i++) {
+			arr[arr_i] = in.nextInt();
+		}
+		long total = 0;
+		for (int k : arr) {
+			total += (long) k;
+		}
+		System.out.println(total);
+	}
+}
---
+++ src/HackerRank/Algorithms/warmup/SimpleArraySum.java
@@ -0,0 +1,22 @@
+/* https://www.hackerrank.com/challenges/simple-array-sum */
+
+package HackerRank.Algorithms.warmup;
+
+import java.util.Scanner;
+
+public class SimpleArraySum {
+	public static void main(String[] args) {
+		int sum = 0;
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int arr[] = new int[n];
+		for (int arr_i = 0; arr_i < n; arr_i++) {
+			arr[arr_i] = in.nextInt();
+		}
+		for (int j : arr) {
+			sum += j;
+		}
+		System.out.println(sum);
+	}
+}
