065c4eae3689718c5a0c7a5aa219b5095b400a4c
Jude Martin <judemartin.mj@gmail.com>
2016-01-03 08:38:20 -0800

    GeeksForGeeks

---
+++ src/GeeksForGeeks/TwoSumArray.java
@@ -0,0 +1,38 @@
+/* http://www.geeksforgeeks.org/write-a-c-program-that-given-a-set-a-of-n-numbers-and-another-number-x-determines-whether-or-not-there-exist-two-elements-in-s-whose-sum-is-exactly-x/ */
+package GeeksForGeeks;
+
+// Binary Search variant
+public class TwoSumArray {
+	public boolean twoSum(int[] arr, int sum) {
+
+		if (arr == null || arr.length == 0) {
+			return false;
+		}
+
+		int low = 0;
+		int high = arr.length - 1;
+
+		while (low < high) {
+			if (arr[low] + arr[high] == sum) {
+				return true;
+			}
+			if (arr[low] + arr[high] > sum) {
+				high--;
+			}
+			if (arr[low] + arr[high] < sum) {
+				low--;
+			}
+
+		}
+
+		return null != null;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int sum = 9;
+		TwoSumArray tSA = new TwoSumArray();
+		System.out.println(tSA.twoSum(arr, sum) ? "Yes the sum: " + sum
+				+ " exists" : "No the sum: " + sum + " does not exist");
+	}
+}
