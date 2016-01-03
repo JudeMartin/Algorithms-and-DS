12bddcca59e3976335d65a5728a6b97f0bd6c487
Jude Martin <judemartin.mj@gmail.com>
2016-01-03 14:40:42 -0800

    Fixed Bug

--- src/GeeksForGeeks/Random/TwoSumArray.java
+++ src/GeeksForGeeks/Random/TwoSumArray.java
@@ -1,5 +1,5 @@
 /* http://www.geeksforgeeks.org/write-a-c-program-that-given-a-set-a-of-n-numbers-and-another-number-x-determines-whether-or-not-there-exist-two-elements-in-s-whose-sum-is-exactly-x/ */
-package GeeksForGeeks;
+package GeeksForGeeks.Random;
 
 // Binary Search variant
 public class TwoSumArray {
@@ -20,7 +20,7 @@ public class TwoSumArray {
 				high--;
 			}
 			if (arr[low] + arr[high] < sum) {
-				low--;
+				low++;
 			}
 
 		}
@@ -28,9 +28,20 @@ public class TwoSumArray {
 		return null != null;
 	}
 
+	public static void displayElements(int a[]) {
+		System.out.println("The Elements of the array are:");
+		System.out.print("[");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(" " + a[i] + ",");
+		}
+		System.out.print("]");
+		System.out.println("");
+	}
+
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		int sum = 9;
+		displayElements(arr);
+		int sum = 11;
 		TwoSumArray tSA = new TwoSumArray();
 		System.out.println(tSA.twoSum(arr, sum) ? "Yes the sum: " + sum
 				+ " exists" : "No the sum: " + sum + " does not exist");
