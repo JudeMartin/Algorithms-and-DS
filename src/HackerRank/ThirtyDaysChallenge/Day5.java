08657d00e2b255933e8f986adc555f898f857798
Jude Martin <judemartin.mj@gmail.com>
2016-01-08 11:55:26 -0800

    HackerRank - Day 5

--- src/HackerRank/ThirtyDaysChallenge/Day5.java
+++ src/HackerRank/ThirtyDaysChallenge/Day5.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 
 public class Day5 {
 	public void looping(int a, int b, int N) {
-		int sum = a;
+		/*int sum = a;
 		for (int i = 0; i < N; i++) {
 			sum += ((int) Math.pow(2, i) * b);
 			System.out.print(sum + " ");
 		}
+		
+		*/
+		
 	}
 
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
+		/*Scanner scn = new Scanner(System.in);
 		int a = scn.nextInt();
 		int b = scn.nextInt();
 		int N = scn.nextInt();
 		Day5 dy5 = new Day5();
-		dy5.looping(a, b, N);
+		dy5.looping(a, b, N);*/
+		Scanner s = new Scanner(System.in);
+		for (int i = 0,c = s.nextInt(); i < c; ++i) {
+             for (int k=0,a=s.nextInt(),b=s.nextInt(),n=s.nextInt(); k<n ;++k)
+               System.out.print((a += b * (int)Math.pow(2, k)) + " ");
+               System.out.println();
+         }
 	}
 }
