859f0c5c744528f79514fd46c1baa3f72af7d43f
Jude Martin <judemartin.mj@gmail.com>
2016-01-09 09:18:44 -0800

    HackerRank  - Day9

---
+++ src/HackerRank/ThirtyDaysChallenge/Day9.java
@@ -0,0 +1,41 @@
+/* https://www.hackerrank.com/contests/30-days-of-code/challenges/day-9-recursion */
+package HackerRank.ThirtyDaysChallenge;
+
+import java.util.Scanner;
+
+public class Day9 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int gcd = find_gcd(a, b);
+		System.out.println(gcd);
+		int gcdEuclid = find_gcd_euclid_style(a, b);
+		System.out.println(gcdEuclid);
+	}
+
+	/* Dikstra's Algorithm for GCD */
+	/* keep subracting the numbers */
+	static int find_gcd(int a, int b) {
+		// Write the base condition
+		if (a == b) {
+			return a;
+		} else if (a > b) {
+			return find_gcd(a - b, b);
+		} else {
+			return find_gcd(a, b - a);
+		}
+	}
+
+	/* euclid's algorithm */
+	/* % operation = Euclid */
+	static int find_gcd_euclid_style(int a, int b) {
+		int min = Math.min(a, b);
+		int max = Math.max(a, b);
+		if (max % min == 0) {
+			return min;
+		} else {
+			return find_gcd_euclid_style(min, max % min);
+		}
+	}
+}
