6c8d1794bc14db4c9bde2c41a2c9ae50f374d710
Jude Martin <judemartin.mj@gmail.com>
2016-01-10 20:13:54 -0800

    HackerRank - Day 5 CodeCleaned

--- src/HackerRank/ThirtyDaysChallenge/Day5.java
+++ src/HackerRank/ThirtyDaysChallenge/Day5.java
@@ -4,29 +4,24 @@ package HackerRank.ThirtyDaysChallenge;
 import java.util.Scanner;
 
 public class Day5 {
-	public void looping(int a, int b, int N) {
-		/*int sum = a;
+	public static void looping(int a, int b, int N) {
+		int sum = a;
 		for (int i = 0; i < N; i++) {
 			sum += ((int) Math.pow(2, i) * b);
 			System.out.print(sum + " ");
 		}
-		
-		*/
-		
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
-		/*Scanner scn = new Scanner(System.in);
-		int a = scn.nextInt();
-		int b = scn.nextInt();
-		int N = scn.nextInt();
-		Day5 dy5 = new Day5();
-		dy5.looping(a, b, N);*/
-		Scanner s = new Scanner(System.in);
-		for (int i = 0,c = s.nextInt(); i < c; ++i) {
-             for (int k=0,a=s.nextInt(),b=s.nextInt(),n=s.nextInt(); k<n ;++k)
-               System.out.print((a += b * (int)Math.pow(2, k)) + " ");
-               System.out.println();
-         }
+		Scanner scn = new Scanner(System.in);
+		int numberOfTestCases = scn.nextInt();
+		while (numberOfTestCases > 0) {
+			int a = scn.nextInt();
+			int b = scn.nextInt();
+			int N = scn.nextInt();
+			looping(a, b, N);
+			numberOfTestCases--;
+		}
 	}
 }
