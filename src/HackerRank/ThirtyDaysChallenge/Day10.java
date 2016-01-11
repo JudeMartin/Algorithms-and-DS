7c553a1bc526c7aa3731e05b0d8bc09211657286
Jude Martin <judemartin.mj@gmail.com>
2016-01-10 17:12:21 -0800

    HackerRank - Day10

---
+++ src/HackerRank/ThirtyDaysChallenge/Day10.java
@@ -0,0 +1,29 @@
+package HackerRank.ThirtyDaysChallenge;
+
+import java.util.Scanner;
+
+public class Day10 {
+	public static String generateBinary(int n) {
+		if (n == 0) {
+			return "0";
+		}
+		String binary = "";
+		while (n > 0) {
+			int rem = n % 2;
+			binary = binary + rem;
+			n = n / 2;
+		}
+		return new StringBuffer(binary).reverse().toString();
+	}
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scn = new Scanner(System.in);
+		int totalNumberofCases = scn.nextInt();
+		while (totalNumberofCases > 0) {
+			String res = generateBinary(scn.nextInt());
+			System.out.println(res);
+			totalNumberofCases--;
+		}
+	}
+}
