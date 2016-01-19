cedc93576ec77d95b544a2ef28ece23e861bf1a7
JudeMartin <judemartin.mj@gmail.com>
2016-01-19 00:07:15 -0800

    LeetCode  - IsPowerOf3 code clean up

--- src/Leetcode/Easy/IsPow.java
+++ src/Leetcode/Easy/IsPow.java
@@ -1,14 +1,19 @@
+/* https://leetcode.com/problems/power-of-three/ */
 package Leetcode.Easy;
 
 public class IsPow {
 	public static boolean isPowerOfThree(int n) {
-		if (n > 1)
-			while (n % 3 == 0)
+		if (n > 1) {
+			while (n % 3 == 0) {
 				n /= 3;
+			}
+		}
 		return n == 1;
 	}
+
 	public static void main(String[] args) {
-		boolean res = isPowerOfThree(81);
-		System.out.println(res);
+		int num = 81;
+		System.out.println(isPowerOfThree(81) ? "Yes " + num + " is a power of 3"
+				: "No " + num + " is not a power of 3");
 	}
 }
