7a2e8dd7d33e69361ca4deec8361da0b0a781a36
Jude Martin <judemartin.mj@gmail.com>
2016-01-21 12:30:49 -0800

    HackerRank - Day17

---
+++ src/HackerRank/ThirtyDaysChallenge/Day17.java
@@ -0,0 +1,18 @@
+/* https://www.hackerrank.com/contests/30-days-of-code/challenges/day-17-exceptions */
+package HackerRank.ThirtyDaysChallenge;
+
+public class Day17 {
+	class Calculator {
+		Calculator() {
+
+		}
+
+		int power(int n, int p) throws Exception {
+			if (n < 0 || p < 0) {
+				throw new Exception("n and p should be non-negative");
+			}
+
+			return (int) Math.pow(n, p);
+		}
+	}
+}
