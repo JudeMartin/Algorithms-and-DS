bc38ee7f07c021bef553e2123dedf52a5f21529c
Jude Martin <judemartin.mj@gmail.com>
2016-01-25 23:24:57 -0800

    HackerRank - Day22

---
+++ src/HackerRank/ThirtyDaysChallenge/Day22.java
@@ -0,0 +1,33 @@
+package HackerRank.ThirtyDaysChallenge;
+
+public class Day22 {
+	class Node {
+		Node left, right;
+		int data;
+
+		Node(int data) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+
+	public static int getHeight(Node root) {
+		// Write your code here
+		if (root == null) {
+			return 0;
+		}
+		int leftHeight = getHeight(root.left);
+		int rightHeight = getHeight(root.right);
+		return Math.max(leftHeight, rightHeight) + 1;
+	}
+
+}
+/*
+ * Get the left height and the right height -> recursively 
+ * 	then add one taking the root element into consideration.
+ * 	finally max of the left and the right heights
+ * ***** Breaking condition is when reaching the last element , hence it becomes null - return as zero    **
+ * 
+ * 
+ * */
+ 
