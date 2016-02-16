3bdfaa945c0087f357a778913e74e49d28b98af7
Jude Martin <judemartin.mj@gmail.com>
2016-02-15 23:14:35 -0800

    Binary Tree Skeleton Code - Mission Peace

---
+++ src/MissionPeace/Trees/BinaryTree.java
@@ -0,0 +1,40 @@
+package MissionPeace.Trees;
+
+class NodeRef {}
+
+enum Color {}
+
+class Node {
+	Node newNode(int data) {
+		return null;
+	}
+}
+
+public class BinaryTree {
+	Node addNode(int data,Node head){
+		return null;
+	}
+	class IntegerRef{
+	}
+	
+	public int height(Node root){
+		return 0;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
+
+/* Methods in Node class */
+	//newNode(what to add)
+
+/* Methods in Binary Tree class */
+	// addNode(what to add, where to start)
+	// height(where to start)
+/* Enum's and ref */
+	// color : red,black
+	// NodeRef : Node node
+
+/* main */
+	// head as null
+	// addNode pass the value and the head ptr. 
