a32f55ac9d278553a90faf979401442bc953e4ce
Jude Martin <judemartin.mj@gmail.com>
2016-02-16 19:43:07 -0800

    Level Order Traversal - Skeleton

---
+++ src/MissionPeace/Trees/LevelOrderTraversal.java
@@ -0,0 +1,13 @@
+package MissionPeace.Trees;
+
+public class LevelOrderTraversal {
+	
+	public void levelOrderTraversal(Node root){
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+}
+/* https://github.com/mission-peace/interview/blob/master/src/com/interview/tree/LevelOrderTraversal.java */
