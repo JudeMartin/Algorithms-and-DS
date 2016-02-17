3f733c2b65d8a69ba21c3213c6d44a47d49eea20
Jude Martin <judemartin.mj@gmail.com>
2016-02-16 20:17:13 -0800

    Added Root Null Check: Level Order Traversal Implementation

--- src/MissionPeace/Trees/LevelOrderTraversal.java
+++ src/MissionPeace/Trees/LevelOrderTraversal.java
@@ -6,6 +6,10 @@ import java.util.Queue;
 public class LevelOrderTraversal {
 
 	public void levelOrderTraversal(Node root) {
+		if (root == null) {
+			System.out.println("Enter a valid Tree!");
+			return;
+		}
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.offer(root);
 		while (!queue.isEmpty()) {
@@ -24,13 +28,13 @@ public class LevelOrderTraversal {
 		LevelOrderTraversal lot = new LevelOrderTraversal();
 		BinaryTree bt = new BinaryTree();
 		Node head = null;
-		head = bt.addNode(10, head);
+		/*head = bt.addNode(10, head);
 		head = bt.addNode(15, head);
 		head = bt.addNode(5, head);
 		head = bt.addNode(7, head);
 		head = bt.addNode(19, head);
 		head = bt.addNode(20, head);
-		head = bt.addNode(-1, head);
+		head = bt.addNode(-1, head);*/
 		System.out.println("Printing the Level Order of the Tree:");
 		lot.levelOrderTraversal(head);
 	}
