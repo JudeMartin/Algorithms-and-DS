95ceff5db7da7c50eb88b38a480d0754992e4bc8
Jude Martin <judemartin.mj@gmail.com>
2016-02-18 23:26:18 -0800

    Added Tree Traversal

---
+++ src/MissionPeace/Trees/TreeTraversals.java
@@ -0,0 +1,10 @@
+public class TreeTraversals {
+	public void inOrder(Node root){
+		if(root == null){
+			return;
+		}
+		inOrder(root.left);
+		System.out.print(root.data + " ");
+		inOrder(root.right);
+	}
+}
