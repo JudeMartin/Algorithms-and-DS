f90a0fa96577300654d0f17f2fb3bcc9abfa1f7f
Jude Martin <judemartin.mj@gmail.com>
2016-02-20 01:48:22 -0800

    Added inOrder Recursive Implementation

--- src/MissionPeace/Trees/TreeTraversals.java
+++ src/MissionPeace/Trees/TreeTraversals.java
@@ -1,12 +1,31 @@
 package MissionPeace.Trees;
 
 public class TreeTraversals{
-	public void InorderRecursive(TreeNode root){}
-	public void InorderIterative(TreeNode root){}
-	public void PreorderRecursive(TreeNode root){}
-	public void PostorderRecursive(TreeNode root){}
+	TreeNode root;
+	
+	public TreeTraversals(){
+		root = null;
+	}
+	
+	public void preOrderRecursive(TreeNode root){
+		if(root == null){
+			return;
+		}
+		System.out.print(root.data+ " ");
+		preOrderRecursive(root.left);
+		preOrderRecursive(root.right);	
+	}
+	public void inOrderRecursive(TreeNode root){}	
+	public void postOrderRecursive(TreeNode root){}
 	public void LevelorderIteravtive(TreeNode root){}
 	public static void main(String[] args){
-		TreeTraversals tt = new TreeTraversals();
+		
+		TreeTraversals tree = new TreeTraversals();
+			tree.root = new TreeNode(3);
+			tree.root.left = new TreeNode(2);
+			tree.root.right = new TreeNode(4);
+			tree.root.left.left = new TreeNode(5);
+			tree.root.left.right = new TreeNode(7);
+			tree.preOrderRecursive(tree.root);
 	}
 }
