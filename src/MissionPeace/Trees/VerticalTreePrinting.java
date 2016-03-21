0fc753649a0755cfa7d4aab6dd9fae0fce47668b
Jude Martin <judemartin.mj@gmail.com>
2016-03-20 23:38:17 -0700

    Vertical Tree Printing

---
+++ src/MissionPeace/Trees/VerticalTreePrinting.java
@@ -0,0 +1,10 @@
+public class VerticalTreePrinting {
+	printVertical(Node root); /* Pass the Tree */
+	printLevel(Map<Integer,List<Node>> map); /* print the map of Integer key and List as the values  */
+	populateMap(); /* Pass the Tree, the map and the level  */
+	public static void main (String[] args){
+		/* Factory -> Tree  */
+		/* Call the printVertical on the factory  */
+	}
+}
+
