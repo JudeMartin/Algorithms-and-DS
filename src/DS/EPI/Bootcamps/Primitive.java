231480a328a953591efa576ae8dd9ee613f73ea0
Jude Martin <judemartin.mj@gmail.com>
2016-04-06 11:48:17 -0700

    DataStructures & Algo's : EPI

---
+++ src/DS/EPI/Bootcamps/Primitive.java
@@ -0,0 +1,18 @@
+package DS.EPI.Bootcamps;
+
+/**
+ * Created by jude on 4/6/16.
+ */
+public class Primitive {
+
+    public void andOperation(){
+        int andOperation = 6 & 4;
+        System.out.print("And Operation of 6 , 4 `is : " + andOperation);
+    }
+
+    public static void main(String[] args){
+        Primitive p = new Primitive();
+        p.andOperation();
+    }
+
+}
