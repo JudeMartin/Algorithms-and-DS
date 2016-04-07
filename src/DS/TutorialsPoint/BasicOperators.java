7bda95eefc140ea5456747c0da5698bfb70304d4
Jude Martin <judemartin.mj@gmail.com>
2016-04-07 00:59:15 -0700

    Basic Operator's Theory

---
+++ src/DS/TutorialsPoint/BasicOperators.java
@@ -0,0 +1,23 @@
+package DS.TutorialsPoint;
+
+/**
+ * Created by jude on 4/6/16.
+ */
+public class BasicOperators {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+/** There are 6 basic operator's
+ * ARB -> LAM
+ * 1) Arithematic : +, -, *, /, %, ++, --; < Addition, Subtraction, Multiplication, Division, Modulos, Increment, Decrement > (7)
+ * 2) Relational  :  ==, !=, <, >, <=, >=; < EqualTo, NotEqualTo, LessThan, GreaterThan, LessThanEqualTo, GreaterThanEqualTo > (6)
+ * 3) Bitwise : &, |, ^, ~, <<, >>, >>>; < Bitwise AND, Bitwise OR,Bitwise XOR, Bitwise Complement, LeftShift, RightShift, ZeroFillRightShift > (7)
+ *
+ * 4) Logical : &&,||, !; < Logical AND, Logical OR, Logical NOT > (3)
+ * 5) Assignment : =, +=, -=, *=, /=, %=, <<=, >>=, &=, ^=, |=  < .... > (11)
+ * 6) Misc :  ? :, instanceof < conditional operator if then else , boolean check > (2)
+**/
