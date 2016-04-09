d2207783687b8b373e44f5b6d6b1e7f298397870
Jude Martin <judemartin.mj@gmail.com>
2016-04-08 23:45:28 -0700

    LeetCode : Two Sum DataStructure Design

---
+++ src/DS/Leetcode/Program Creek/a.StringArray/TwoSumDataStructureDesign.java
@@ -0,0 +1,34 @@
+package a.StringArray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jude on 4/6/16.
+ */
+public class TwoSumDataStructureDesign {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public void add(int number){
+        if(map.containsKey(number)){
+            map.put(number,map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+        }
+    }
+
+    public boolean find(int number){
+        for (Integer i : maps.keySet()){
+            int target = number - i;
+            if(map.containsKey(target)){
+                if(i == target && map.get(target) < 2){
+                    continue;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
