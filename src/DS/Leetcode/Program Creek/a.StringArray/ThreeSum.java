7d7da9ab23d6fc242be82869b9ed925c98f9e442
Jude Martin <judemartin.mj@gmail.com>
2016-04-08 23:58:59 -0700

    ThreeSum & FourSum

---
+++ src/DS/Leetcode/Program Creek/a.StringArray/ThreeSum.java
@@ -0,0 +1,18 @@
+package a.StringArray;
+
+
+import java.util.List;
+
+/**
+ * Created by jude on 4/6/16.
+ */
+public class FourSum {
+
+    public List<List<Integer>> fourSum(int[] nums,int target){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
