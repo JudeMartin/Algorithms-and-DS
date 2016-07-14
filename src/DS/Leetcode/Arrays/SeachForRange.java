dad4fedb0a0ec6b371f00d26f5f90b310db40543
Jude Martin <judemartin.mj@gmail.com>
2016-07-13 22:18:20 -0700

    Search for a Range

---
+++ src/DS/Leetcode/Arrays/SeachForRange.java
@@ -0,0 +1,15 @@
+/* https://leetcode.com/problems/search-for-a-range/ */
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int idx = Arrays.binarySearch(nums, target);
+        if (idx < 0) {
+            return new int[]{-1, -1};
+        }
+        int left = idx - 1, right = idx + 1;
+        while (left >= 0 && nums[left] == nums[idx])
+            left--;
+        while (right < nums.length && nums[right] == nums[idx])
+            right++;
+        return new int[]{left + 1, right - 1};
+    }
+}
