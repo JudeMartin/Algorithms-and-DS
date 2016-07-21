f497a28f8300dd392c6982d98891604d1265a6ba
Jude Martin <judemartin.mj@gmail.com>
2016-07-21 01:49:54 -0700

    Spiral Matrix 2

--- src/DS/Leetcode/Arrays/SpiralMatrix2.java
+++ src/DS/Leetcode/Arrays/SpiralMatrix2.java
@@ -2,26 +2,26 @@
 public class Solution {
     public int[][] generateMatrix(int n) {
         int[][] ret = new int[n][n];
-	int left = 0,top = 0;
-	int right = n -1,down = n - 1;
+	int left = 0, top = 0;
+	int right = n - 1, down = n - 1;
 	int count = 1;
 	while (left <= right && top <= down) {
 		for (int j = left; j <= right; j ++) {
 			ret[top][j] = count++;
 		}
-		top ++;
+		top++;
 		for (int i = top; i <= down; i ++) {
-			ret[i][right] = count ++;
+			ret[i][right] = count++;
 		}
-		right --;
+		right--;
 		for (int j = right; j >= left; j --) {
-			ret[down][j] = count ++;
+			ret[down][j] = count++;
 		}
-		down --;
+		down--;
 		for (int i = down; i >= top; i --) {
-			ret[i][left] = count ++;
+			ret[i][left] = count++;
 		}
-		left ++;
+		left++;
 	}
 	return ret;
     }
