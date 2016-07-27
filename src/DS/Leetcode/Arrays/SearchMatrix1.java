c1edf99f27359f468a8e87431e30668631973b32
Jude Martin <judemartin.mj@gmail.com>
2016-07-26 22:18:28 -0700

    Search Matrix 1 using Binary Search

---
+++ src/DS/Leetcode/Arrays/SearchMatrix1.java
@@ -0,0 +1,22 @@
+/* https://leetcode.com/problems/search-a-2d-matrix/ */
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == matrix[mid / n][mid % n]) {
+                return true;
+            } else if (target < matrix[mid / n][mid % n]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        
+        }
+        return false;
+    }
+}
