c826e084dc67379c75049e281ec9d8c7775712f4
Jude Martin <judemartin.mj@gmail.com>
2016-08-04 13:14:44 -0700

    Diameter of the Binary Tree

--- src/DS/GeeksForGeeks/Tree/TreeDimensions.java
+++ src/DS/GeeksForGeeks/Tree/TreeDimensions.java
@@ -1,6 +1,5 @@
 package DS.GeeksForGeeks.Tree;
 
-
 public class TreeDimensions {
 
 	public static int size(TreeNode root) {
@@ -12,8 +11,8 @@ public class TreeDimensions {
 	}
 
 	public static void deleteTree(TreeNode root) {
-		//use postOrder traversal to delete the tree 
-		if(root == null) {
+		// use postOrder traversal to delete the tree
+		if (root == null) {
 			return;
 		}
 		deleteTree(root.left);
@@ -42,8 +41,21 @@ public class TreeDimensions {
 		return countLeaves(root.right) + countLeaves(root.left);
 	}
 
-	public int diameter(TreeNode root) {
-		return 0;
+	public static int diameter(TreeNode root) {
+		// find the leftHeight, rHeight for the root
+		// repeat the leftHeight and rightHeight for every level
+		// check if the root is included in the diameter
+		if (root == null) {
+			return 0;
+		}
+		int lHeight = maxDepthOrHeight(root.left);
+		int rHeight = maxDepthOrHeight(root.right);
+
+		int lDiameter = diameter(root.left);
+		int rDiameter = diameter(root.right);
+
+		return Math
+				.max((lHeight + rHeight + 1), Math.max(lDiameter, rDiameter));
 	}
 
 	public static void main(String[] args) {
@@ -56,8 +68,10 @@ public class TreeDimensions {
 		System.out.println("The size of the Binary Tree is: " + size(root));
 		System.out.println("The Max Depth of height of the Binary Tree is: "
 				+ maxDepthOrHeight(root));
+		System.out.println("The Diameter of the given tree: " + diameter(root));
 		System.out.println("The number of leaves of the Binary Tree is: "
 				+ countLeaves(root));
 		deleteTree(root);
+
 	}
 }
